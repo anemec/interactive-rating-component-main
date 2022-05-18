1ec90debbfa153b1cbce44b5c7fc349a0b991648
Andrew Nemec <andrewnemec2@gmail.com>
2022-05-18 13:33:43 -0500

    Added rating card and number selection logic

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const submitted = document.getElementById('submitted-card');
+const rating = document.getElementById('rating-card');
+const submitBtn = document.getElementById('submit');
+const target = document.getElementById('target');
+let ratingNumber = 0;
+
+submitBtn.addEventListener('click', () => {
+  submitted.classList.remove('hidden');
+  rating.classList.add('hidden');
+  target.innerText = `You selected ${ratingNumber} out of 5`;
+});
+
+function setRating(rating) {
+  ratingNumber = rating;
+  console.log(ratingNumber);
+}
