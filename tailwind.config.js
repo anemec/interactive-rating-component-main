cf2e265006dcd52fbb60b134607dc8bc6dda472a
Andrew Nemec <andrewnemec2@gmail.com>
2022-05-18 11:38:28 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        orangePrim: 'hsl(25, 97%, 53%)',
+        whiteNeutral: 'hsl(0, 0%, 100%)',
+        lightGreyNeutral: 'hsl(217, 12%, 63%)',
+        mediumGreyNeutral: 'hsl(216, 12%, 54%)',
+        darkBlueNeutral: 'hsl(213, 19%, 18%)',
+        veryDarkBlueNeutral: 'hsl(216, 12%, 8%)',
+      },
+    },
+  },
+  plugins: [],
+};
